1351719d0df5d24d84d0bbf355a3fd427728fed6
Sheel Patel <sheelpatel44887@gmail.com>
2026-06-19 22:17:03 +0530

    models module added

---
+++ backend/app/models/models_utils.py
@@ -0,0 +1,11 @@
+import os
+
+APP_DIR = 'app'
+MODEL_DIR = 'models'
+MODEL_NAME = 'ExtraTrees.joblib'
+
+MODEL_PATH = os.path.join(APP_DIR , MODEL_DIR)
+MODEL_PATH = os.path.join(MODEL_PATH , MODEL_NAME)
+
+
+
